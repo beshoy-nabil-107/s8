9f41776bd25bcfdaa54be2dc2f633b0a8cc2ae1c
beshoy-nabil-107 <beshoynabil107@gmail.com>
2025-12-28 01:39:36 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './assets/pages/Home'
+import Profile from './assets/pages/Profile'
+import NotFound from './assets/pages/NotFound'
+
+export default function App() {
+  return (
+    <div className='app-container'>
+      <div className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+
+      </div>
+
+
+      <div className="main-container">
+
+        <Routes>
+          <Route path='/' Component={Home} />
+          <Route path='/profile' Component={Profile} />
+          <Route path='*' Component={NotFound} />
+
+
+
+        </Routes>
+
+      </div>
+
+
+    </div>
+  )
+}
